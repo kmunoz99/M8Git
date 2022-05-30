30068a31c093d4b8fe6c7757ff461b9dd7aa1884
arnaubdnaaru <arnaubdnaaru@gmail.com>
2022-05-30 19:41:13 +0200

    test

--- test.php
+++ test.php
@@ -1,136 +1,83 @@
 <?php 
-// *
-// * @author arnau
-class Matriu
-{
-    private $matriu = array();
-    
-    function  __construct(&$matriu)
-    {
-        $this->matriu = $matriu;
+/**
+ * @author arnau
+ */
+
+include "Matriu.php";
+
+$m1 = array();
+$m2 = array();
+
+ for($i = 0; $i < 3; $i = $i +1){
+    for($j = 0; $j < 3; $j = $j +1){
+        $m1[$i][$j] = $i;
+        $m2[$i][$j] = $j;
     }
+ }
 
-    function sumaMatrius($m1)
-    {
-        $matriuResultat = null;
-        if (count($m1->matriu) == count($this->matriu) && count($m1->matriu[0]) == count($this->matriu[0]))
-        {
-            for ($i = 0; $i < count($m1->matriu); $i++)
-            {
-                for ($j = 0; $j < count($m1->matriu[$i]); $j++)
-                {
-                    $matriuResultat[$i][$j] = $this->matriu[$i][$j] + $m1->matriu[$i][$j];
-                }
-            }
-        }
-        $matriuARetornar = new Matriu($matriuResultat);
-        return $matriuARetornar;
-    }
+ $matriu1 = new Matriu($m1);
+ $matriu2 = new Matriu($m2);
 
-    function restaMatrius($m1)
-    {
-        $matriuResultat = null;
-        if (count($m1->matriu) == count($this->matriu) && count($m1->matriu[0]) == count($this->matriu[0]))
-        {
-            for ($i = 0; $i < count($m1->matriu); $i++)
-            {
-                for ($j = 0; $j < count($m1->matriu[$i]); $j++)
-                {
-                    $matriuResultat[$i][$j] = $this->matriu[$i][$j] - $m1->matriu[$i][$j];
-                }
-            }
-        }
-        $matriuARetornar = new Matriu($matriuResultat);
-        return $matriuARetornar;
-    }
+ $matriu1->showMatriu();
 
-    function multiplicaMatriuPerEscalar($escalar)
-    {
-        $matriuResultat = null;
-        for ($i = 0; $i < count($this->matriu); $i++)
-        {
-            for ($j = 0; $j < count($this->matriu[$i]); $j++)
-            {
-                $matriuResultat[$i][$j] = $this->matriu[$i][$j] * $escalar;
-            }
-        }
-        $matriuARetornar = new Matriu($matriuResultat);
-        return $matriuARetornar;
-    }
+ echo "   +   <br>";
 
-    function producteDuesMatrius($m1)
-    {
-        $matriuResultat = array_fill(0,count($this->matriu),array_fill(0,count($m1->matriu[0]),0.0));
-        if (count($this->matriu[0]) == count($m1->matriu))
-        {
-            for ($i = 0; $i < count($this->matriu); $i++)
-            {
-                for ($j = 0; $j < count($m1->matriu[0]); $j++)
-                {
-                    for ($k = 0; $k < count($this->matriu[0]); $k++)
-                    {
-                        $matriuResultat[$i][$j] += $this->matriu[$i][$k] * $m1->matriu[$k][$j];
-                    }
-                }
-            }
-        }
-        $matriuARetornar = new Matriu($matriuResultat);
-        return $matriuARetornar;
-    }
+ $matriu2->showMatriu();
 
+ echo "   =   <br>";
 
-    function matriuIdentitat($n){
-        $I = array();
-        for($i = 0; $i < $n; $i = $i + 1){
-            for($j = 0; $j < $n; $j = $j + 1){
-                if($i == $j){
-                    $I[$i][$j] = 1;
-                }else{
-                    $I[$i][$j] = 0;
-                }
-            }
-        }
-        return $I;
-    }
+ $matriu3 = $matriu1->sumaMatrius($matriu2);
 
-    function matriuInvertida($debug = TRUE)
-    {
-        /// @todo check rows = columns
+ $matriu3->showMatriu();
 
-        $n = count($this->matriu);
+ echo "<br>-------------------------------<br>";
 
-        // get and append identity matrix
-        $I = $this->matriuIdentitat($n);
-        for ($i = 0; $i < $n; ++ $i) {
-            $this->matriu[$i] = array_merge($this->matriu[$i], $I[$i]);
-        }
+ $matriu1->showMatriu();
 
-        return $I;
-    }
+ echo "  -  <br>";
 
+ $matriu2->showMatriu();
 
-    function showMatriu()
-    {
-        for ($i = 0; $i < count($this->matriu); $i++)
-        {
-            for ($j = 0; $j < count($this->matriu[$i]); $j++)
-            {
-                printf(" %.2f |",$this->matriu[$i][$j]);
-            }
-            echo "<br>";
-        }
-    }
+ echo "  =   <br>";
 
-    function __toString(){
-        
-        $m = "";
-        for ($i = 0; $i < count($this->matriu); $i++)
-        {
-            for ($j = 0; $j < count($this->matriu[$i]); $j++)
-            {
-                $m += "$this->matriu[$i][$j] | ";
-            }
-            echo "<br>";
-        }
-    }
-}
+ $matriu4 = $matriu1->restaMatrius($matriu2);
+
+ $matriu4->showMatriu();
+
+ echo "<br>-------------------------------<br>";
+
+ $matriu1->showMatriu();
+
+ echo "  * 4 = <br>";
+
+ $matriu5 = $matriu1->multiplicaMatriuPerEscalar(4);
+
+ $matriu5->showMatriu();
+
+ 
+ echo "<br>-------------------------------<br>";
+
+ $matriu1->showMatriu();
+
+ echo "  *  <br>";
+
+ $matriu2->showMatriu();
+
+ echo "  =   <br>";
+
+ $matriu6 = $matriu1->producteDuesMatrius($matriu2);
+
+ $matriu6->showMatriu();
+
+ echo "<br>-------------------------------<br>";
+
+ $matriu1->showMatriu();
+
+ echo "  Inversa = <br>";
+
+ $m1 = $matriu1->matriuInvertida();
+
+ $matriu7 = new Matriu($m1);
+
+ $matriu7->showMatriu();
+?>
